736d3a1ee82e691b3ad70062cc1d3535aeb938d3
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2023-12-06 17:45:23 -0400

    Updates

---
+++ assets/AboutPage-0d28dafe.js
@@ -0,0 +1,22 @@
+import{a as o,j as e}from"./index-35de3ae6.js";const d=[{title:"“English Word”",content:`
+        ”English Words” is the Top English Learning Web App about words on English, based on the idea of knowing the principal concepts of a word for to get better on Vocabulary, Pronunciation and Listening, also, the central purpose of “English Words” is to solve the next problem:
+        
+
+
+        - ‘I want to learn more vocabulary but i don’t want to go to youtube or other web; to know the Definition, Pronunciation or to look sentences of this word - I want to have all of this, in just one Web’.
+        
+
+
+        This is “English Word”, a Helper to improve your vocabulary on English.`,img_src:o.englishWordAbout,img_title:"imagen que representa English Words",h2_title:"English Word”"},{title:"About Me :)",content:`
+        Hi :) I’m Roddy Rafael Tejeda Rosario
+        
+
+
+        I am a creative Full-Stack Developer [ I like more the Front-End ;) ] from the Dominican Republic, I like to create complete Web Apps that provide solutions to real problems.
+        
+
+
+        Versatile developer with expertise in solving real-world problems. Worked in both front-end and back-end development for a software consultancy, creating visually appealing and user-friendly web applications. Enjoys reading, drawing, and writing in free time.`,img_src:o.myImgAbout,img_title:"imagen del dev de English Words",h2_title:"The Dev of “English Word”"}],c=({title:t,content:n,img_src:l,img_title:s,h2_title:i})=>e.jsxs(e.Fragment,{children:[e.jsx("div",{children:e.jsxs("h2",{className:" text-center text-base sm:text-3xl p-2 pt-7 font-semibold text-white",children:["📃 About ",e.jsx("span",{className:"bg-white p-1 rounded-lg font-bold text-blue-500",children:i})," 📃"]})}),e.jsx("div",{className:"p-2 sm:p-6 pb-10 lg:p-5",children:e.jsxs("div",{className:`\r
+             max-w-full lg:max-w-full lg:h-96 lg:flex shadow-lg shadow-blue-900/30`,children:[e.jsx("div",{className:`\r
+                h-48 lg:h-96 lg:w-80 bg-cover bg-center rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden flex-none`,style:{backgroundImage:`url('${l}')`},title:s}),e.jsx("div",{className:"bg-white rounded-b sm:p-4 pt-6 lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal",children:e.jsxs("div",{children:[e.jsxs("h2",{className:"text-blue-900 font-bold mb-2 text-center text-base sm:text-3xl mb-5 ",children:["⭐⭐",e.jsx("span",{className:"bg-blue-500 text-white p-2 rounded-lg mx-2",children:t}),"⭐⭐"]}),e.jsx("div",{className:"bg-blue-50 rounded-lg shadow-inner m-3 p-4",children:n.split(`
+`).map((r,a)=>e.jsx("p",{className:"mb-4 text-base text-blue-900 font-semibold",children:r},a))})]})})]})})]});function h(){return e.jsx(e.Fragment,{children:e.jsx("div",{children:d.map((t,n)=>e.jsx(c,{...t},n))})})}function m(){return e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"bg-blue-500",children:e.jsx(h,{})}),e.jsx("div",{children:e.jsx("h2",{className:"text-center mt-6 p-3 font-bold text-blue-500 text-2xl sm:text-3xl lg:text-3xl xl:text-4xl",children:"✨ i'll help you with the Success of your projects ✨"})})]})}export{m as default};
